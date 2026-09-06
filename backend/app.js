ef38b9e601e6fb3ff3cc2228ccf13129e3e8a48c
soyreach <soyreach009@gmail.com>
2026-09-06 09:44:46 +0700

    Changed the cors origin

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 const allowedOrigins = [
-  "http://localhost:5173",
+  "https://pos-system-frontend-liard.vercel.app/",
   process.env.FRONTEND_URL,
 ].filter(Boolean);
 
